b04ab50112c8a69facf78896ec00d0a4e9e1fc05
Deepak <deepak25mb@gmail.com>
2019-05-26 11:43:51 +0530

    Updates

---
+++ precache-manifest.af08353ce0c6751d322d43d0681e9b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d97d671e77d5e433bd35db0484dbc5a",
+    "url": "/2-speech_synthesis/index.html"
+  },
+  {
+    "revision": "6b05cdb2b01acceffc63",
+    "url": "/2-speech_synthesis/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "118e5dc86410aa33dbbe",
+    "url": "/2-speech_synthesis/static/js/2.9141f8de.chunk.js"
+  },
+  {
+    "revision": "6b05cdb2b01acceffc63",
+    "url": "/2-speech_synthesis/static/js/main.b0b0e445.chunk.js"
+  },
+  {
+    "revision": "64320ec177c951c14114",
+    "url": "/2-speech_synthesis/static/js/runtime~main.a258bef4.js"
+  }
+]);
